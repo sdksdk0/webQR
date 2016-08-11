bb37c8a30dccc9b61703cedc65af3a4e0c0ef41a
sdksdk0 <690644511@qq.com>
2016-08-11 22:31:48 +0800

    commit

---
+++ src/cn/tf/utils/JdbcUtil.java
@@ -0,0 +1,59 @@
+package cn.tf.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class JdbcUtil {
+	private static String driverClass;
+	private static String url;
+	private static String user;
+	private static String password;
+	static{
+		ResourceBundle rb = ResourceBundle.getBundle("dbinfo");
+		driverClass = rb.getString("driverClass");
+		url = rb.getString("url");
+		user = rb.getString("user");
+		password = rb.getString("password");
+		try {
+			Class.forName(driverClass);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws Exception{
+		Connection conn = DriverManager.getConnection(url,user,password);
+		return conn;
+	}
+
+	public static void release(ResultSet rs,Statement stmt,Connection conn){
+		if(rs!=null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			rs = null;
+		}
+		if(stmt!=null){
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			stmt = null;
+		}
+		if(conn!=null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			conn = null;
+		}
+	}
+}
